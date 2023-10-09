d036e1886578079177ce57837556a0e2207052ed
dui-luis-alberto <lgarciaorozco6@gmail.com>
2023-10-09 12:55:29 -0600

    n list

--- maximun.py
+++ maximun.py
@@ -5,8 +5,13 @@
 # LICENSE GNU/GPL
 # 9/10/23
 
+from random import randint
 
-data = [15.4,551.5,5.5,68.4,6.0]
+n = 1e6
+a = -1e5
+b = 1e5
+
+data = [randint(int(a),int(b)) for _ in range(int(n))]
 m = None
 if data:
     m = data[0]
--- minimum.py
+++ minimum.py
@@ -6,11 +6,18 @@
 # 9/10/23
 
 
-data = [15.4,551.5,5.5,68.4,6.0]
-m = None
+from random import randint
+
+n = 1e6
+a = -1e5
+b = 1e5
+
+data = [randint(int(a),int(b)) for _ in range(int(n))]
+
 if data:
     m = data[0]
     for x in data:
         if x <= m: m = x
+else: m = None
 
 print(m)
